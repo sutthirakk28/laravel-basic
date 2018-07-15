02888ef97c50f447e1fa561cb78b8c9fed136f7d
sutthirak <sutthirak.k28@gmail.com>
2018-07-15 22:36:25 +0700

    แก้ไขวันภาษาไทย

--- resources/views/lib/show.blade.php
+++ resources/views/lib/show.blade.php
@@ -4,42 +4,43 @@
 @endsection
 @section('content')
 
-  
+
   @php
     function getAge($birthday) {
       $then = strtotime($birthday);
       return(floor((time()-$then)/31556926));
-    }     
+    }
 
     function getDate1($day) {
       $diff  = date_diff( date_create($day), date_create() );
       return($diff->y.' ปี '.$diff->m.' เดือน '.$diff->d.' วัน');
     }
 
-    $thai_day_arr=array("อาทิตย์","จันทร์","อังคาร","พุธ","พฤหัสบดี","ศุกร์","เสาร์");
-    $thai_month_arr=array(
-      "0"=>"",
-      "1"=>"มกราคม",
-      "2"=>"กุมภาพันธ์",
-      "3"=>"มีนาคม",
-      "4"=>"เมษายน",
-      "5"=>"พฤษภาคม",
-      "6"=>"มิถุนายน", 
-      "7"=>"กรกฎาคม",
-      "8"=>"สิงหาคม",
-      "9"=>"กันยายน",
-      "10"=>"ตุลาคม",
-      "11"=>"พฤศจิกายน",
-      "12"=>"ธันวาคม"                 
-    );
     function thai_date($time){
-        global $thai_day_arr,$thai_month_arr;
-        $thai_date_return="วัน".$thai_day_arr[date("w",$time)];
-        $thai_date_return.= "ที่ ".date("j",$time);
-        $thai_date_return.=" เดือน".$thai_month_arr[date("n",$time)];
-        $thai_date_return.= " พ.ศ.".(date("Y",$time)+543);
-        return $thai_date_return;
+      $thai_day_arr=array("อาทิตย์","จันทร์","อังคาร","พุธ","พฤหัสบดี","ศุกร์","เสาร์");
+      $thai_month_arr=array(
+        "0"=>"",
+        "1"=>"ม.ค.",
+        "2"=>"ก.พ.",
+        "3"=>"มี.ค.",
+        "4"=>"เม.ย.",
+        "5"=>"พ.ค.",
+        "6"=>"มิ.ย.",
+        "7"=>"ก.ค.",
+        "8"=>"ส.ค.",
+        "9"=>"ก.ย.",
+        "10"=>"ต.ค.",
+        "11"=>"พ.ย.",
+        "12"=>"ธ.ค."
+      );
+      $thai_date_return="วัน ".$thai_day_arr[date("w",$time)];
+      $thai_date_return.= " ที่ ".date("j",$time);
+      $thai_date_return.=" ".$thai_month_arr[date("n",$time)];
+      $thai_date_return.= " ".(date("Y",$time)+543);
+      //$thai_date_return.= "  ".date("H:i",$time)." น.";
+      return $thai_date_return;
     }
+
   @endphp
   @foreach ($lib as $libs)
   <div class="panel panel-primary">
@@ -74,27 +75,28 @@
         </tr>
         <tr>
           <td class="div4"><span >วันเริ่มงาน</span></td>
-          <td><span class="div3">{{ $libs['job_start']}}</span></td>
-        </tr>
-        <tr>
-          <td class="div4"><span >ครบ 45 วัน</span></td>          
-          <td><span class="div3">{{ date('d/m/Y',strtotime('+60 days',strtotime('07/14/2018')))}}</span></td>
-        </tr>
-        <tr class="div5">
-          <td class="div4"><span >ครบ 60 วันส่งเอกสารการประเมิน</span></td>
-          <td><span class="div3">{{ date('d/m/Y',strtotime('+60 days',strtotime('07/14/2018'))) }}</span></td>
-        </tr>
-        <tr class="div5">
-          <td class="div4"><span >กำหนดรับเอกสารประเมิน</span></td>
           <td><span class="div3">{{ thai_date(strtotime($libs['job_start'])) }}</span></td>
         </tr>
         <tr>
+          <td class="div4"><span >ครบ 45 วัน</span></td>
+          <td><span class="div3">{{ thai_date(strtotime('+45 days',strtotime($libs['job_start']))) }}</span></td>
+        </tr>
+        <tr class="div5">
+          <td class="div4"><span >ครบ 60 วันส่งเอกสารการประเมิน</span></td>
+          <td><span class="div3">{{ thai_date(strtotime('+60 days',strtotime($libs['job_start']))) }}</span></td>
+        </tr>
+        <tr class="div5">
+
+          <td class="div4"><span >กำหนดรับเอกสารประเมิน</span></td>
+          <td><span class="div3">{{ thai_date(strtotime('+10 days',strtotime($libs['job_start']))) }}</span></td>
+        </tr>
+        <tr>
           <td class="div4"><span >ครบ 90 วัน</span></td>
-          <td><span class="div3">{{ $libs['job_start']}}</span></td>
+          <td><span class="div3">{{ thai_date(strtotime('+90 days',strtotime($libs['job_start']))) }}</span></td>
         </tr>
         <tr class="div6">
           <td class="div4"><span >ผ่านทดลองาน 119 วัน</span></td>
-          <td><span class="div3">{{ $libs['job_start']}}</span></td>
+          <td><span class="div3">{{ thai_date(strtotime('+119 days',strtotime($libs['job_start']))) }}</span></td>
         </tr>
         <tr >
           <td class="div4"><span >อายุงาน</span></td>
